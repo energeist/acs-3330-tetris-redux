e4fd8f60cb42a2df09a243e4cef28775d620499b
Mark Rattle <mark.rattle@gmail.com>
2024-02-11 20:50:23 -0500

    added grid square

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import './App.css';
+
+import GridSquare from './components/GridSquare'
 
 function App() {
   return (
@@ -6,6 +9,15 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <GridSquare color="1" />
+      <GridSquare color="2" />
+      <GridSquare color="3" />
+      <GridSquare color="4" />
+      <GridSquare color="5" />
+      <GridSquare color="6" />
+      <GridSquare color="7" />
+      <GridSquare color="8" />
+      <GridSquare color="0" />
     </div>
   );
 }
