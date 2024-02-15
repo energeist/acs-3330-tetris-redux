18a2b17b04aac65d65adf49b9e23eaf91c6e3606
Mark Rattle <mark.rattle@gmail.com>
2024-02-15 11:28:32 -0500

    added connection for controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,28 +1,49 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice'
 
 export default function Controls(props) {
+  const dispatch = useDispatch()
+  const { isRunning } = useSelector(state => state)
+  
   return (
-  <div className="controls">
-    {/* left */}
-    <button className="control-button" onClick={(e) => {
-      // ...
-    }}>Left</button>
+    <div className="controls">
+      {/* left */}
+      <button 
+        disabled={!isRunning}
+        className="control-button" 
+        onClick={(e) => {
+          dispatch(moveLeft())
+        }
+      }>Left</button>
 
-    {/* right */}
-    <button className="control-button" onClick={(e) => {
-      // ...
-    }}>Right</button>
+      {/* right */}
+      <button 
+        disabled={!isRunning}
+        className="control-button" 
+        onClick={(e) => {
+          dispatch(moveRight())
+        }
+      }>Right</button>
 
-    {/* rotate */}
-    <button className="control-button" onClick={(e) => {
-      // ...
-    }}>Rotate</button>
+      {/* rotate */}
+      <button 
+        disabled={!isRunning}
+        className="control-button" 
+        onClick={(e) => {
+          dispatch(rotate())
+        }  
+      }>Rotate</button>
 
-    {/* down */}
-    <button className="control-button" onClick={(e) => {
-      // ...
-    }}>Down</button>
-
-  </div>
+      {/* down */}
+      <button 
+        disabled={!isRunning}
+        className="control-button" 
+        onClick={(e) => {
+          dispatch(moveDown())
+        }
+      }>Down</button>
+    </div>
   )
 }
